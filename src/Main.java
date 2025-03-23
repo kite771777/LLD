7872bca9b315b458eb57169526de450b10205dcd
Ritik Raj <ritikraj@Ritiks-MacBook-Air.local>
2025-03-24 00:21:12 +0530

    inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-public class Main {
+import oops.Animal;
+
+public class Main extends Animal {
     public static void main(String[] args) {
+        Animal dog =new Animal();
+        dog.eat();
         System.out.println("Hello, World!");
     }
 }

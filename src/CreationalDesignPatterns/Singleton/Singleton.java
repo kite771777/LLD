733fc81a626d9f6439e4c4dc0c939e0142755779
Ritik Raj <ritikraj@Ritiks-MacBook-Air.local>
2025-07-29 03:32:30 +0530

    singleton sync design pattern

--- src/CreationalDesignPatterns/Singleton/Singleton.java
+++ src/CreationalDesignPatterns/Singleton/Singleton.java
@@ -2,7 +2,6 @@ package CreationalDesignPatterns.Singleton;
 
 public class Singleton {
     private static Singleton instance;
-
     // 1. Private constructor
     private Singleton() {
         System.out.println("Singleton instance created");
@@ -11,7 +10,11 @@ public class Singleton {
     // 2. Public static method to provide access
     public static Singleton getInstance() {
         if (instance == null) {
-            instance = new Singleton(); // Lazy initialization
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton(); // Lazy initialization
+                }
+            }
         }
         return instance;
     }

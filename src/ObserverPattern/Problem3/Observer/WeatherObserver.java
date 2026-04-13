fa23a111c5a53364f81d8bbfba561f9c725ec319
Ritik Raj <rajgitritik@gmail.com>
2026-04-14 02:52:51 +0530

    added a observer design pattern code

---
+++ src/ObserverPattern/Problem3/Observer/WeatherObserver.java
@@ -0,0 +1,7 @@
+package ObserverPattern.Problem3.Observer;
+// Observer interface - defines the update method
+// Concrete observers implement this interface to update their state
+// and respond to changes in its OWN way
+public interface WeatherObserver {
+    void update();
+}
